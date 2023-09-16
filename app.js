4ff43ef07780ce5f720b2cf8624b399a5859b92e
weverson <weversonlek@gmail.com>
2023-09-15 21:23:20 -0300

    Novas interações

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
 const app =express();
 const handlebars  = require("express-handlebars");
+const bodyParser = require('body-parser');
 const Sequelize = require("sequelize");
 
 
 //config
+
     //Tamplate engine
-    app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
+
+        app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
         app.set('view engine','handlebars');
+
+
+    // body parser
+
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+
+
     //conexão com mysql
-        const sequelize = new Sequelize('livro','root','',{
+
+        const sequelize = new Sequelize('livro','root','1F@miliasempre',{
             host:"localhost",
             dialect:"mysql",
         }); 
@@ -20,7 +32,7 @@ app.get('/cad',(req,res) =>{
 });
 
 app.post('/add',(req,res) => {
-    res.send('Formuladio recebido!..')
+    res.send('Seu nome:' + req.body.titulo + " e "+ req.body.conteudo)
 })
 
 
